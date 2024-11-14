37599a1efb7985c24905a2545abee281ec76d135
Alef5750 <alewitt@gmail.com>
2024-11-14 16:59:03 +0200

    shuffle cards randomly in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import "./App.css";
 import { itemData } from "./data";
 import { Gameboard } from "./Gameboard";
+import { IImageItem } from "./types";
 
 function App() {
+  const shuffle = (array: IImageItem[]) => {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      const temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+    return array;
+  };
+  const shuffledData = shuffle(itemData);
+
   return (
     <>
-      <Gameboard itemData={itemData} />
+      <Gameboard itemData={shuffledData} />
     </>
   );
 }
